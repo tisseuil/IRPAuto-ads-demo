9000ba2a8b96854275532ce4f12a982350d9f52c
zaouam <zaouam.sirageddine@hotmail.com>
2023-03-30 15:39:09 +0200

    test

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -3,9 +3,3 @@ SELECT  IND.Siret_CODE, IND.Etablissement_ID, concat(' A POUR:',TYP.LibelleSourc
 	INNER JOIN dbo.T_Dim_RelationPersonnes RLT ON RLT.PersonneSource_FID=IND.Etablissement_ID 
 	INNER JOIN dbo.T_Dim_TypeRelation TYP ON TYP.TypeRelation_ID=RLT.TypeRelation_FID 
 	Where IND.Siret_CODE = 'Z1485258520015'
-	
-
---Z1485258520015
---B0123456789123
---W1485215210015
---W1485215210015
